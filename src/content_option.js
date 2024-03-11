a910d9034a60a93ad171ad33e068c7ed89688606
malinisaran <malinisaran199@gmail.com>
2024-03-11 22:32:07 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "SS";
+const meta = {
+    title: "Saran Srini",
+    description: "Im Saran Srinivasan",
+};
+
+const introdata = {
+    title: "I’m Saran Srini",
+    animated: {
+        first: "Website Development",
+        second: "Game Design",
+        third: "Block-Chain",
+        fouth: "Photography",
+        fifth: "Video Editing",
+        sixth: "Music Composing",
+    },
+    description: "pursuing Computer Science in my sophomore year",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: " Myself Saran Srinivasan V from Tamilnadu ,India. Presuing Computer Science in my Sopomora Year",
+};
+const worktimeline = [{
+        jobtitle: "Will be Updated",
+        where: "Will be Updated",
+        date: "Will be Updated",
+    },
+    {
+        jobtitle: "Will be Updated",
+        where: "Will be Updated",
+        date: "Will be Updated",
+    },
+    {
+        jobtitle: "Will be Updated",
+        where: "Will be Updated",
+        date: "Will be Updated",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 70,
+    },
+    {
+        name: "Django",
+        value: 50,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Figma",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Design",
+        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Game Design",
+        // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "Coming soon!",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "saransrini003@gmail.com", 
+    YOUR_FONE: "8072872929",
+    description: "Feel free to contact or join with us ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/SaranSrini03",
+    facebook: "https://www.instagram.com/saransrini_/",
+    linkedin: "https://www.linkedin.com/in/saran-srini-3993a7251/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
